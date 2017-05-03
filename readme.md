aa4f8fa8c0da601c722330f6dc0e4cf5fe329a5f
tendertime <531764426@qq.com>
2017-05-03 22:34:23 +0800

    init

---
+++ readme.md
@@ -0,0 +1,24 @@
+目前剩余部分:
+1:登录/验证环节:
+用户的登录/注册; 
+房间的创建/进入;
+(重复部分较多,建议直接建立密码函数对其调用);
+2:文件:
+上传/获取;
+
+操作:
+1.node app.js
+2.npm install
+注意:npm xx --save 保留一下版本号,有需要的时候注意--devolpe
+安装好mongoose后一路进入到mongodb下继续npm install 我也不知道为什么
+bower install --allow-root
+
+出了问题请@stackoverflow  有的坑万一我没踩过呢.
+
+关于git用法:
+自己百度去.
+请务必记得忽略掉bower的文件:./static/components 以及./node_modules
+
+
+
+
---
+++ static/technode.js
@@ -0,0 +1,28 @@
+angular.module('techNodeApp', ['ngRoute', 'angularMoment'])
+  .run(function ($window, $http, $rootScope, $location) {
+    $window.moment.lang('zh-cn')
+    //consolog.('1')
+    $http({
+      url: '/ajax/validate',
+      method: 'GET'
+    }).success(function (user) {
+      $rootScope.me = user
+      if ($location.path() == '/login') {
+        $location.path('/rooms')
+      }
+    }).error(function (data) {
+      $location.path('/login')
+    })
+    $rootScope.logout = function() {
+      $http({
+        url: '/ajax/logout',
+        method: 'GET'
+      }).success(function () {
+        delete $rootScope.me
+        $location.path('/login')
+      })
+    }
+    $rootScope.$on('login', function (evt, me) {
+      $rootScope.me = me
+    })
+  })
